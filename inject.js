13c6e4351b870ca398f6ec42eb8d933ee55867c1
Russell Pate <russ@russpate.com>
2018-02-12 18:20:04 -0500

    modifying selection range

--- inject.js
+++ inject.js
@@ -22,11 +22,11 @@
             .addClass('lightning');
       },
       incomplete: function(){
-          $('.tabularReportTable td:nth-child(2n+9):contains(Not Done)')
+          $('.tabularReportTable td:nth-child(n+2):nth-child(-n+14):contains(Not Done)')
           .addClass('lightning');
       },
       complete: function(){
-          $('.tabularReportTable td:nth-child(2n+9):contains(Complete)')
+          $('.tabularReportTable td:nth-child(n+2):nth-child(-n+14):contains(Complete)')
             .addClass('boom');
       }
   }
