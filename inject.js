6ce8a355eba6f36f012ac76549ad4e60bbdd3ac0
Russell Pate <russ@russpate.com>
2018-02-12 16:20:48 -0500

    selected td range for newly created complete vs incomplete status. add new style for complete and incomplete. updated lightening. refactored js slightly.

--- inject.js
+++ inject.js
@@ -1,7 +1,33 @@
+// (function() {
+//   $(document).ready(function() {
+//     // Use this at https://realthread.my.salesforce.com/00OE0000002MBFV
+//     $('.tabularReportTable td:nth-child(2).nowrapCell:contains(1)')
+//       .addClass('lightning');
+//   });
+//
+// })();
 (function() {
   $(document).ready(function() {
-    // Use this at https://realthread.my.salesforce.com/00OE0000002MBFV
-    $('.tabularReportTable td:nth-child(2).nowrapCell:contains(1)')
-      .addClass('lightning');
+      // Use this at https://realthread.my.salesforce.com/00OE0000002MBFV
+    rtHighlight.init();
   });
+  const rtHighlight = {
+      init: function(){
+          rtHighlight.priority();
+          rtHighlight.incomplete();
+          rtHighlight.complete();
+      },
+      priority: function(){
+          $('.tabularReportTable td:nth-child(2).nowrapCell:contains(1)')
+            .addClass('lightning');
+      },
+      incomplete: function(){
+          $('.tabularReportTable td:nth-child(2n+9):contains(Not Done)')
+          .addClass('lightning');
+      },
+      complete: function(){
+          $('.tabularReportTable td:nth-child(2n+9):contains(Complete)')
+            .addClass('boom');
+      }
+  }
 })();
