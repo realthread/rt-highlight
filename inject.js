d3d3df80681c7e99147ea9718b9f46dbb7502157
Russell Pate <russ@russpate.com>
2018-02-12 13:53:35 -0500

    remove style from tr and added style to td only

--- inject.js
+++ inject.js
@@ -2,7 +2,6 @@
   $(document).ready(function() {
     // Use this at https://realthread.my.salesforce.com/00OE0000002MBFV
     $('.tabularReportTable td:nth-child(2).nowrapCell:contains(1)')
-      .closest('tr')
       .addClass('lightning');
   });
 })();
