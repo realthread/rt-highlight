1e15d02a5a67e9e4fe9167462349970b15cee112
Russell Pate <russ@russpate.com>
2018-02-13 13:32:45 -0500

    refactor

--- inject.js
+++ inject.js
@@ -1,33 +1,58 @@
 // (function() {
 //   $(document).ready(function() {
-//     // Use this at https://realthread.my.salesforce.com/00OE0000002MBFV
-//     $('.tabularReportTable td:nth-child(2).nowrapCell:contains(1)')
-//       .addClass('lightning');
+//       // Use this at https://realthread.my.salesforce.com/00OE0000002MBFV
+//     rtHighlight.init();
 //   });
 //
+//   const rtHighlight = {
+//   //     init: function(){
+//   //         rtHighlight.priority();
+//   //         rtHighlight.incomplete();
+//   //         rtHighlight.complete();
+//   //     },
+//   //     priority: function(){
+//   //         $('.tabularReportTable td:nth-child(2).nowrapCell:contains(1)')
+//   //           .addClass('lightning');
+//   //     },
+//   //     incomplete: function(){
+//   //         $('.tabularReportTable td:nth-child(n+3):nth-child(-n+14):contains(Not Done)')
+//   //         .addClass('lightning');
+//   //     },
+//   //     complete: function(){
+//   //         $('.tabularReportTable td:nth-child(n+3):nth-child(-n+14):contains(Complete)')
+//   //           .addClass('boom');
+//   //     }
+//   // }
 // })();
+
 (function() {
   $(document).ready(function() {
-      // Use this at https://realthread.my.salesforce.com/00OE0000002MBFV
     rtHighlight.init();
   });
-  const rtHighlight = {
-      init: function(){
-          rtHighlight.priority();
-          rtHighlight.incomplete();
-          rtHighlight.complete();
+  // immediately invoking function expression -- iife
+  var checkArr = false;
+  console.log('top Arr = ' + checkArr);
+
+  var rtHighlight = {
+      init: function() {
+          rtHighlight.setHighlight([2,13],'1', 'lightning');
       },
-      priority: function(){
-          $('.tabularReportTable td:nth-child(2).nowrapCell:contains(1)')
-            .addClass('lightning');
-      },
-      incomplete: function(){
-          $('.tabularReportTable td:nth-child(n+3):nth-child(-n+14):contains(Not Done)')
-          .addClass('lightning');
-      },
-      complete: function(){
-          $('.tabularReportTable td:nth-child(n+3):nth-child(-n+14):contains(Complete)')
-            .addClass('boom');
+      setHighlight: function(range, searchString, classNameToAdd) {
+          console.log('range is ' + range)
+          if (Array.isArray(range)){
+              checkArr = true;
+              console.log('this is an Array');
+          }
+          console.log('outside Arr = ' + checkArr);
+          if (checkArr){
+              $('.tabularReportTable td').slice(range[0], range[1]).find('td').filter(function(){
+                  return $(this).text() === searchString;
+              }).addClass(classNameToAdd); console.log(range);
+          } else {
+              $('.tabularReportTable td').eq(range).find('.nowrapCell').filter(function(){
+                  return $(this).text() === searchString;
+              }).addClass(classNameToAdd); console.log('cat');
+          }
       }
   }
 })();
