a270b32b3d9d3980592a954ab27e58b552ca5078
Jake LeBoeuf <dev@jakeleboeuf.com>
2018-02-13 19:39:38 -0500

    adds inverse method

--- inject.js
+++ inject.js
@@ -1,34 +1,38 @@
 // Use this at https://realthread.my.salesforce.com/00OE0000002MBFV
-(function() {
-  $(document).ready(function() {
+(function () {
+  $(document).ready(function () {
     rtHighlight.init();
   });
 
   var rtHighlight = {
     init() {
-      rtHighlight.setHighlight([3, 14], 'Not Done', 'lightning');
-      rtHighlight.setHighlight([3, 14], 'Complete', 'boom');
+      rtHighlight.setHighlight([11, 13], 'Complete', 'boom');
+      rtHighlight.setHighlight([11, 13], 'Complete', 'lightning', true);
       rtHighlight.setHighlight(2, '1', 'lightning');
     },
-    getElements(range, searchString) {
+    getElements(range, searchString, exclude) {
       if (Array.isArray(range)) {
         return $('.tabularReportTable tr')
           .find(
-            'td:nth-child(n+' + range[0] + '):nth-child(-n+' + range[1] + ')'
+            '> td:nth-child(n+' + range[0] + '):nth-child(-n+' + range[1] + '):not(.nowrapCell)'
           )
-          .filter(function() {
-            return $(this).text() === searchString;
+          .filter(function () {
+            if (exclude) {
+              return $(this).text() !== searchString;
+            } else {
+              return $(this).text() === searchString;
+            }
           });
       } else {
         return $(
           '.tabularReportTable td:nth-child(' + range + ').nowrapCell'
-        ).filter(function() {
+        ).filter(function () {
           return $(this).text() === searchString;
         });
       }
     },
-    setHighlight(range, searchString, classNameToAdd) {
-      rtHighlight.getElements(range, searchString).addClass(classNameToAdd);
+    setHighlight(range, searchString, classNameToAdd, exclude) {
+      rtHighlight.getElements(range, searchString, exclude).addClass(classNameToAdd);
     }
   };
 })();
